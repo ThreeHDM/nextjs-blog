0e033b7c354d223df2374baabfa76b9b6a7f926d
ThreeHDM <juanbilardi@gmail.com>
2021-10-23 12:10:13 -0300

    Meta component created and applied to all pages

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,6 +1,7 @@
 import { server } from "../../../config";
 import Link from 'next/Link'
 import { useRouter } from "next/router"
+import Meta from '../../../components/Meta'
 
 const Article = ({article}) => {
     // const router = useRouter()
@@ -8,6 +9,7 @@ const Article = ({article}) => {
 
     return (
         <>
+            <Meta title={article.title} description={article.excerpt} />
             <h1>{article.title}</h1>
             <p>{article.body}</p>
             <Link href='/'>Go Back</Link>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
 import { server } from "../config";
-import Head from "next/head";
 import ArticleList from "../components/ArticleList";
 
 const Home = ({ articles }) => {
   return (
     <div>
-      <Head>
-        <title>WebDev Newz</title>
-        <meta name="keywords" content="web development, programming" />
-      </Head>
-
       <ArticleList articles={articles}/>
     </div>
   );
