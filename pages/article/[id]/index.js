76491bf25cb111cc54536199ce4bcb70f2f812cd
ThreeHDM <juanbilardi@gmail.com>
2021-10-23 11:59:24 -0300

    json placeholder replaced with custom data api

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,3 +1,4 @@
+import { server } from "../../../config";
 import Link from 'next/Link'
 import { useRouter } from "next/router"
 
@@ -15,7 +16,7 @@ const Article = ({article}) => {
 }
 
 export const getStaticProps = async context => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const res = await fetch(`${server}/api/articles/${context.params.id}`)
 
     const article = await res.json()
 
@@ -27,7 +28,7 @@ export const getStaticProps = async context => {
 }
 
 export const getStaticPaths  = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const res = await fetch(`${server}/api/articles`)
 
     const articles = await res.json();
 
@@ -40,6 +41,5 @@ export const getStaticPaths  = async () => {
         fallback: false
     }
 }
-
 
 export default Article
